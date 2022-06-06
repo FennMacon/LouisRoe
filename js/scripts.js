8a245c496c6193f6da3000938f80c82e60e51fac
FennMacon <fennmacon@gmail.com>
2022-06-06 15:10:17 -0400

    style update

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,6 @@
-var mb = document.getElementById("imgmb").style;
-var tv = document.getElementById("imgtv").style;
-var sp = document.getElementById("imgsp").style;
-var ow = document.getElementById("imgow").style;
 const head = document.getElementById("topname");
 let namecount = 0
 
-function togglemb() {
-    if (mb.display == "none" || mb.display == "") {
-        mb.display = "flex";
-    } else {
-        mb.display = "none";
-    }
-}
-
-function toggletv() {
-    if (tv.display == "none" || tv.display == "") {
-        tv.display = "block";
-    } else {
-        tv.display = "none";
-    }
-}
-
-function togglesp() {
-    if (sp.display == "none" || sp.display == "") {
-        sp.display = "flex";
-    } else {
-        sp.display = "none";
-    }
-}
-
-function toggleow() {
-    if (ow.display == "none" || ow.display == "") {
-        ow.display = "block";
-    } else {
-        ow.display = "none";
-    }
-}
 
 function namechange() {
     const fonts = ['Roboto', 'Roboto Mono', 'Arial', 'Verdana', 'Helvetica', 'Tahoma', 'Trebuchet MS', 'Times New Roman', 'Georgia', 'Garamond', 'Courier New', 'Brush Script MT', 'Comic Sans'];
